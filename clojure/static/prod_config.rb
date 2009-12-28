99a012ce962e83ea1d0f71f99bc37c1facc6dd7e
Baishampayan Ghose <b.ghose@infinitelybeta.com>
2009-12-28 15:26:55 +0530

    Initial commit

---
+++ clojure/static/prod_config.rb
@@ -0,0 +1,17 @@
+# Path to common SASS files
+add_import_path "/Users/brajeshwar/src/sass"
+
+# compass plugins
+require 'susy'
+
+project_type = :stand_alone
+
+# paths
+http_path = ".."
+images_dir = "i"
+sass_dir = "sass"
+css_dir = "css"
+javascripts_dir = "js"
+
+# output option: nested, expanded, compact, compressed
+output_style = :compressed
